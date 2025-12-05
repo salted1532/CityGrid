55a679db3dc831dbaf83e72de6f2d0b40fa85a9b
Salted1532 <salted1532@naver.com>
2025-12-05 19:19:02 +0900

    그리드 시스템 초기 부분 제작
    쉘 단위로 움직이는 마우스커서 제작
    기초적인 쉘이 인게임에서 보이도록 제작

---
+++ Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+// --------------------------------------------------
+// InputManager는 플레이어가 작동시키는 모든 인풋을 감지하고 요청을 보내는 매니저입니다.
+// --------------------------------------------------
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField]
+    private Camera sceneCamera;
+
+    private Vector3 lastPosition;
+
+    [SerializeField]
+    private LayerMask placementLayerMask;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Vector3 GetSelectedMapPosition()
+    {
+        Vector2 mousePos = Mouse.current.position.ReadValue();
+
+        Ray ray = sceneCamera.ScreenPointToRay(mousePos);
+        if (Physics.Raycast(ray, out RaycastHit hit, 100, placementLayerMask))
+        {
+            lastPosition = hit.point;
+        }
+
+        return lastPosition;
+    }
+
+}
---
+++ Assets/Scripts/Systems/PlacementSystem.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PlacementSystem : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject mouseIndicator, cellIndicator;
+    [SerializeField]
+    private InputManager inputManager;
+    [SerializeField]
+    private Grid grid;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePosition = inputManager.GetSelectedMapPosition();
+        Vector3Int gridPosition = grid.WorldToCell(mousePosition);
+        mouseIndicator.transform.position = mousePosition;
+        cellIndicator.transform.position = grid.CellToWorld(gridPosition);
+    }
+}
