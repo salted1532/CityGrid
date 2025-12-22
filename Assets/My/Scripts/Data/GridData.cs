18b79a551c4b845e1cbeb44298d171a1ea2f7d7d
Salted1532 <salted1532@naver.com>
2025-12-23 01:27:09 +0900

    도로 인접 건물 설치 가능 기능 추가
    도로 인접된 반경 4칸안에만 건물 설치 가능(크기별로도 체크함)

--- Assets/My/Scripts/Data/GridData.cs
+++ Assets/My/Scripts/Data/GridData.cs
@@ -34,6 +34,44 @@ public class GridData
         }
         return returnVal;
     }
+    public bool HasRoadNearby(Vector3Int position, int radius, Vector2Int objectSize)
+    {
+        // 건물이 차지하는 모든 셀 검사
+        for (int ox = 0; ox < objectSize.x; ox++)
+        {
+            for (int oy = 0; oy < objectSize.y; oy++)
+            {
+                Vector3Int occupiedCell = position + new Vector3Int(ox, 0, oy);
+
+                // 이 occupiedCell 기준으로 반경 검사
+                bool cellHasRoad = false;
+
+                for (int x = -radius; x <= radius; x++)
+                {
+                    for (int y = -radius; y <= radius; y++)
+                    {
+                        Vector3Int checkPos = occupiedCell + new Vector3Int(x, 0, y);
+                        if (placedObjects.ContainsKey(checkPos))
+                        {
+                            cellHasRoad = true;
+                            break;
+                        }
+                    }
+                    if (cellHasRoad) break;
+                }
+
+                // 건물의 한 셀이라도 도로 반경 밖이면 설치 불가
+                if (!cellHasRoad)
+                    return false;
+            }
+        }
+
+        return true; // 전체 셀이 전부 도로 반경 4칸 안에 있음
+    }
+    public List<Vector3Int> CalculatePositionsPublic(Vector3Int gridPosition, Vector2Int objectSize)
+    {
+        return CalculatePositions(gridPosition, objectSize);
+    }
 
     public bool CanPlaceObejctAt(Vector3Int gridPosition, Vector2Int objectSize)
     {
--- Assets/My/Scripts/Systems/PlacementSystem.cs
+++ Assets/My/Scripts/Systems/PlacementSystem.cs
@@ -62,22 +62,41 @@ public class PlacementSystem : MonoBehaviour
         if (inputManager.IsPointerOverUI())
         {
             return;
+            //UI 위에 클릭시 코드 입력 부분
         }
         Vector3 mousePosition = inputManager.GetSelectedMapPosition();
         Vector3Int gridPosition = grid.WorldToCell(mousePosition);
 
-        bool placementValidity = CheckPlacementValidity(gridPosition, selectedObjectIndex);
-        if (placementValidity == false)
+        if (selectedObjectIndex > 0)
         {
-            return;
+            bool RoadValidity = CanPlaceStructure(gridPosition, selectedObjectIndex);
+            bool placementValidity = CheckPlacementValidity(gridPosition, selectedObjectIndex);
+            if (RoadValidity == false)
+            {
+                return;
+            }
+            if(placementValidity == false)
+            {
+                return;
+            }
+        }
+        else if (selectedObjectIndex == 0)
+        {
+            bool placementValidity = CheckPlacementValidity(gridPosition, selectedObjectIndex);
+            if (placementValidity == false)
+            {
+                return;
+            }
         }
 
+        //설치 코드들
         GameObject newObject = Instantiate(database.objectData[selectedObjectIndex].Prefab);
         newObject.transform.position = grid.CellToWorld(gridPosition);
         placedGameObject.Add(newObject);
         GridData selectedData = database.objectData[selectedObjectIndex].ID == 0 ? roadData : StructureData;
         selectedData.AddObjectAt(gridPosition, database.objectData[selectedObjectIndex].Size, database.objectData[selectedObjectIndex].ID, placedGameObject.Count - 1);
         preview.UpdatePosition(grid.CellToWorld(gridPosition), false);
+        Debug.Log(gridPosition);
     }
 
     private bool CheckPlacementValidity(Vector3Int gridPosition, int selectedObjectIndex)
@@ -110,12 +129,48 @@ public class PlacementSystem : MonoBehaviour
 
         if(lastDectectedPosition != gridPosition)
         {
-            bool placementValidity = CheckPlacementValidity(gridPosition, selectedObjectIndex);
-            //previewRenderer.material.color = placementValidity ? Color.white : Color.red;
+            //bool placementValidity = CheckPlacementValidity(gridPosition, selectedObjectIndex);
+            if(selectedObjectIndex > 0)
+            {
+                bool RoadValidity = CanPlaceStructure(gridPosition, selectedObjectIndex);
+                bool placementValidity = CheckPlacementValidity(gridPosition, selectedObjectIndex);
+                bool CheckValidity;
+                if (RoadValidity && placementValidity)
+                {
+                    CheckValidity = true;
+                }
+                else
+                {
+                    CheckValidity = false;
+                }
+                preview.UpdatePosition(grid.CellToWorld(gridPosition), CheckValidity);
+            }
+            else if(selectedObjectIndex == 0)
+            {
+                bool placementValidity = CheckPlacementValidity(gridPosition, selectedObjectIndex);
+                preview.UpdatePosition(grid.CellToWorld(gridPosition), placementValidity);
+            }
             mouseIndicator.transform.position = mousePosition;
+            //previewRenderer.material.color = placementValidity ? Color.white : Color.red;
             //cellIndicator.transform.position = grid.CellToWorld(gridPosition);
-            preview.UpdatePosition(grid.CellToWorld(gridPosition), placementValidity);
+            //preview.UpdatePosition(grid.CellToWorld(gridPosition), placementValidity);
             lastDectectedPosition = gridPosition;
         }
+    }
+
+    private bool CanPlaceStructure(Vector3Int gridPosition, int selectedObjectIndex)
+    {
+        // 건물이면 1) 충돌 검사 2) 도로 반경 검사 둘 다 필요
+        bool noCollision = roadData.CanPlaceObejctAt(
+            gridPosition,
+            database.objectData[selectedObjectIndex].Size);
+
+        if (!noCollision)
+            return false;
+
+        // 도로 반경 4칸 내에 도로가 있는지 검사
+        bool nearRoad = roadData.HasRoadNearby(gridPosition, 4, database.objectData[selectedObjectIndex].Size);
+
+        return nearRoad;
     }
 }
